691676a7d9431bcf890b1e4b454110e4362326e7
Magnificent Meow <magnificent.meow@gmail.com>
2014-08-09 16:16:57 -0400

    Katsu! Database Generator Optional :yum:

--- database/build-database.js
+++ database/build-database.js
@@ -3,27 +3,82 @@ require('shelljs/global');
 var particleDatabase = {};
 
 // For each folder
-ls('*').forEach(function(snippetSetName) {
-    if (test('-d', directory) ){ // yes this is a directory with snippets.
+var designComponents = [];
+
+ls('l*').forEach(function(designComponent) {
+    if (test('-d', designComponent) ){
         
-        var snippetCount = ls(snippetSetName + '/*').length / 4;
+        // yes this is a directory with snippets, yay!
+        designComponents.push(designComponent)
+        
+    }
+});
+designComponents.sort();
+console.log('Encountered %s Component Sets', designComponents.length)
+
+designComponents.forEach(function(designComponent, designComponentIndex) {
+         
+        potentialComponents = []
+        particleDatabase[designComponent] = potentialComponents;
+        
+        // the counter counts 'snip-0-code.html' files as those are the absolute minimum required for a snippet.
+        var snippetCount = ls(designComponent + '/*code.html').length;
+        console.log('Level %s contains a set of %d.', designComponent, snippetCount);
+    
+        for(var snippetNumber=0; snippetNumber<snippetCount; snippetNumber++){
+            var snippet = {};
+            
+            snippet.code  = "";
+            var codeFilePath =  designComponent + '/' + 'snip-'+snippetNumber+'-code.html';
+            if(test("-f",codeFilePath)) {
+                snippet.code  = cat(codeFilePath);
+            }
+            
+            snippet.model = "";
+            var modelFilePath =  designComponent + '/' + 'snip-'+snippetNumber+'-model.json';
+            if(test("-f",modelFilePath)) {
+                snippet.model = cat(modelFilePath);
+            }
+            
+            snippet.start = "";
+            var startFilePath =  designComponent + '/' + 'snip-'+snippetNumber+'-start.js';
+            if(test("-f",startFilePath)) {
+                snippet.start = cat(startFilePath);
+            }
+            
+            snippet.stop  = "";
+            var stopFilePath =  designComponent + '/' + 'snip-'+snippetNumber+'-stop.js';
+            if(test("-f",stopFilePath)) {
+                snippet.stop  = cat(stopFilePath);
+            }
+            
+            snippet.style = "";
+            var styleFilePath =  designComponent + '/' + 'snip-'+snippetNumber+'-style.css';
+            if(test("-f",styleFilePath)) {
+                snippet.style = cat(styleFilePath);
+            }
+            
+            
+            potentialComponents[snippetNumber] = snippet;
+        };
+        
+        
     
         var particleObject = {};
-        particleDatabase[snippetSetName] = particleObject;
+        
+        
+        
+        
     
          
-        particleObject.html = cat('.html');
-        particleObject.css  = cat('.css');
-        particleObject.js   = cat('.js');
-        particleObject.json = cat('.json');
-        
-        var obj = JSON.parse(str);
-        
-        response[file] = obj; 
- 
+       
+
+
+
+
+
    
-    } // is snippet dir?
-});
+ });
 
 var objs = JSON.stringify(particleDatabase, null, '\t');
-objs.to('database.json')
+objs.to('database.json');
